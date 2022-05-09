6a2a64b49e3989bddbb9bd2950bcf22fe177dc01
ezgioflr <ezgioflar@gmail.com>
2022-05-09 10:29:57 +0300

    Login işlemi için Backend yazıldı. Şifremi unuttum işlemi için Backend yazılmaya başlandı.

--- ASMSPresentationLayer/Controllers/AccountController.cs
+++ ASMSPresentationLayer/Controllers/AccountController.cs
@@ -12,6 +12,9 @@ using ASMSEntityLayer.Enums;
 using ASMSEntityLayer.ViewModels;
 using ASMSEntityLayer.ResultModels;
 using ASMSBusinessLayer.ViewModels;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using System.Text.Encodings.Web;
 
 namespace ASMSPresentationLayer.Controllers
 {
@@ -118,6 +121,123 @@ namespace ASMSPresentationLayer.Controllers
 
                 //loglanacak;
                 return RedirectToAction("Error", "Home");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Login(string email)
+        {
+            LoginViewModel model = new LoginViewModel()
+            {
+                Email = email
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                var user = await _userManager.FindByNameAsync(model.Email);
+                //var user = _userManager.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Epostanız ya da şifreniz hatalıdır! Tekrar deneyiniz!");
+                    return View();
+                }
+
+                //TODO : son parametre bool lockOutOnFailure ile ilgili örnek yapalım
+                var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+
+                //TODO : son parametre bool lockOutOnFailure ile ilgili örnek yapalım
+
+                //if (result.IsLockedOut) kullanıcı kilitli ise süre kısıtlaması koyabiliriz.
+                //{
+                //    DateTimeOffset d = user.LockoutEnd.Value;
+                //}
+
+                if (!result.Succeeded)
+                {
+
+                    ModelState.AddModelError("", "Epostanız ya da şifreniz hatalıdır! Tekrar deneyiniz!");
+                    return View();
+                }
+                //Artık hoşgeldi
+
+                if (_userManager.IsInRoleAsync(user, ASMSRoles.Student.ToString()).Result)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                if (_userManager.IsInRoleAsync(user, ASMSRoles.Coordinator.ToString()).Result)
+                {
+                    return RedirectToAction("Dashboard", "Admin");
+                }
+
+                if (_userManager.IsInRoleAsync(user, ASMSRoles.StudentAdministration.ToString()).Result)
+                {
+                    return RedirectToAction("Dashboard", "Admin");
+                }
+                return RedirectToAction("Index", "Home");
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Beklenmedik bir hata oluştu! Tekrar deneyiniz.");
+                //ex loglaması yapılacak
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(string email)
+        {
+            try
+            {
+                var user = await _userManager.FindByEmailAsync(email);
+                if (user == null)
+                {
+                    ViewBag.ResetPasswordSuccessMessage = "Şifre yenileme talebiniz alındı! Epostanızı kontrol ediniz";
+                    return View();
+
+                }
+
+                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                var codeEncode = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callBackUrl = Url.Action("ConfirmResetPassword", "Account", new { userId = user.Id, code = codeEncode },
+                    protocol: Request.Scheme);
+
+                var emailMessage = new EmailMessage()
+                {
+                    Contacts = new string[] { user.Email },
+                    Subject="ASMS- Yeni Şifre Talebi",
+                    Body=$"Merhaba {user.Name}{user.Surname},"+
+                    $"<br/> Yeni parola belirlemek için"+
+                    $"<a href='{HtmlEncoder.Default.Encode(callBackUrl)}'> buraya </a> tıklayınız..."
+                    
+                };
+                await _emailSender.SendMessage(emailMessage);
+                ViewBag.ResetPasswordSuccessMessage = "Şifre yenileme talebiniz alındı! Epostanızı kontrol ediniz!";
+
+                     return View();
+
+            }
+            catch (Exception ex)
+            {
+                //ex loglansın
+                ViewBag.ResetPasswordFailMessage = "Beklenmedik bir hata oluştu! Tekrar deneyiniz!";
+                return View();
             }
         }
     }
